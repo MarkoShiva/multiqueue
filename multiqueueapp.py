e786609900217743baf73b86a8229d6bcfc1adfa
Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>
2019-09-30 16:52:25 +0200

    Hacked stuff so much refactor needed but Ibored af

--- multiqueueapp.py
+++ multiqueueapp.py
@@ -1,6 +1,6 @@
 from PySide2.QtWidgets import QApplication, QMainWindow
 import sys
-import json
+import datetime
 
 # import uic
 from ui_multiqueueapp import Ui_MainWindow
@@ -28,35 +28,80 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.db = QSqlDatabase.addDatabase("QSQLITE")
         self.db.setDatabaseName("store")
         self.db.open()
-        self.que = QSqlQuery("CREATE Table priority (id integer, task varchar(256));")
+        self.que = QSqlQuery("CREATE Table urgent (priority integer, "
+                             "task varchar(256), date datetime);")
         if self.que.exec_:
             print("Executed successful.")
+        self.que = QSqlQuery("CREATE Table priority (priority integer, "
+                             "task varchar(256), date datetime);")
+        if self.que.exec_:
+            print("Executed successful.")
+        self.que = QSqlQuery("CREATE Table dueDate (priority integer, "
+                             "task varchar(256), date datetime);")
+        if self.que.exec_:
+            print("Executed successful.")
+        self.load()
 
     # ToDo add multiple views one for daily list one for a scrum view one for gantt chart agregated view one for calendar view add tracking
-
-
 
 # ToDo rewrite app in MVC mode.
     def save(self):
         date = self.ui.dateEdit.text()
+        # cur = datetime.datetime.now()
         txt = self.ui.lineEdit.text()
         ls = self.ui.comboBox.currentIndex()
         if ls == 0:
             text = date + " | " + self.ui.lineEdit.text()
-            self.ui.priority.addItem(text)
-            que = QSqlQuery("Insert into priority task=" + text + ";")
+            self.ui.urgent.addItem(text)
+            query = "Insert into urgent (priority, task, date) VALUES " \
+                    "(1, '" + text + "', datetime('now') " + " );"
+            print(query)
+            que = QSqlQuery(query)
+
+
             if que.exec_:
                 print("Successful")
+                self.db.commit()
         elif ls == 1:
-            self.ui.noPriority.addItem(date + " | " + self.ui.lineEdit.text())
+            text = date + " | " + self.ui.lineEdit.text()
+            self.ui.priority.addItem(text)
+            query = "Insert into priority (priority, task, date) VALUES " \
+                    "(1, '" + text + "', datetime('now') " + " );"
+            que = QSqlQuery(query)
+            if que.exec_:
+                print("Successful")
+                self.db.commit()
         elif ls == 2:
-            self.ui.dated.addItem(date + " | " + self.ui.lineEdit.text())
+            text = date + " | " + self.ui.lineEdit.text()
+            self.ui.dueDate.addItem(text)
+            query = "Insert into dueDate (priority, task, date) VALUES " \
+                    "(1, '" + text + "', datetime('now') " + " );"
+            que = QSqlQuery(query)
+            if que.exec_:
+                print("Successful")
+                self.db.commit()
         else:
             pass
         self.ui.lineEdit.clear()
 
+    def load(self):
+        query = QSqlQuery()
+        query.exec_("SELECT task from priority;")
+        while query.next():
+            print(query.value(0))
+            self.ui.priority.addItem(query.value(0))
+        query = QSqlQuery()
+        query.exec_("SELECT task from urgent;")
+        while query.next():
+            print(query.value(0))
+            self.ui.urgent.addItem(query.value(0))
+        query = QSqlQuery()
+        query.exec_("SELECT task from dueDate;")
+        while query.next():
+            print(query.value(0))
+            self.ui.dueDate.addItem(query.value(0))
 
-
+#
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
